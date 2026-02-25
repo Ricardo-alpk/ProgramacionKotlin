f41eb62c6787ce200f5ef93076fc4049a870f737
ricardo_alpk <ricardo.man.ramirez@gmail.com>
2026-02-25 21:24:55 +0100

    Actualización de archivos

--- src/Practica_Examen/Ejer7.kt
+++ src/Practica_Examen/Ejer7.kt
@@ -1,35 +1,69 @@
 package practicas
-
+//
 fun main() {
-    print("Introduzca la primera nota: ")
-    val primeraNota : Float = (readLine()?: "0").toFloat()
+    println("Introduce la primera nota")
+    val nota1 : Float = (readLine()?: "0").toFloat()
 
-    print("Introduzca la segunda nota: ")
-    val segundaNota : Float = (readLine()?: "0").toFloat()
+    println("OKEY AHORA CON LA NOTA NUMERO 2")
+    val nota2 : Float = (readLine()?: "0").toFloat()
 
-    print("Introduzca la tercera nota: ")
-    val terceraNota : Float = (readLine()?: "0").toFloat()
+    println("OKEY AHORA CON LA NOTA NUMERO 3")
+    val nota3 : Float = (readLine()?: "0").toFloat()
 
-    print("Introduzca la cuarta nota: ")
-    val cuartaNota : Float = (readLine()?: "0").toFloat()
+    println("OKEY AHORA CON LA NOTA NUMERO 4")
+    val nota4 : Float = (readLine()?: "0").toFloat()
 
-    val notaMedia = (primeraNota + segundaNota + terceraNota + cuartaNota) / 4
-    val mensaje : Any = when {
-        notaMedia >= 7 -> "Notable"
-        notaMedia >= 5 && notaMedia < 7 -> "Aprobado"
-        notaMedia < 5 -> {
-            print("¿El alumno se ha presentado a recuperación? (S/N)")
-            val respuesta : String? = readLine()?.uppercase()
 
-            when (respuesta) {
-                "S" -> "En recuperación"
-                "N" -> "Suspenso"
-                else -> "Respuesta no esperada"
+    val promedio = ((nota1 + nota2 + nota3 + nota4) / 4)
+
+    //println(promedio)
+
+    when{
+        promedio >= 7 -> println("NOTABLE")
+        promedio >=5 && promedio <7 -> println("APROBADO")
+        promedio <5 -> {
+            println("EL ALUMNO SE PRESENTARA A RECUPERACIÓN?")
+            val respuesta : String? = readLine()?.uppercase()?:""
+            when{
+                respuesta == "SI" -> print
             }
         }
-        else -> "Nota no esperada"
     }
-
-    println("Situación del alumno: $mensaje")
-
 }
+
+
+
+
+//fun main() {
+//    print("Introduzca la primera nota: ")
+//    val primeraNota : Float = (readLine()?: "0").toFloat()
+//
+//    print("Introduzca la segunda nota: ")
+//    val segundaNota : Float = (readLine()?: "0").toFloat()
+//
+//    print("Introduzca la tercera nota: ")
+//    val terceraNota : Float = (readLine()?: "0").toFloat()
+//
+//    print("Introduzca la cuarta nota: ")
+//    val cuartaNota : Float = (readLine()?: "0").toFloat()
+//
+//    val notaMedia = (primeraNota + segundaNota + terceraNota + cuartaNota) / 4
+//    val mensaje : Any = when {
+//        notaMedia >= 7 -> "Notable"
+//        notaMedia >= 5 && notaMedia < 7 -> "Aprobado"
+//        notaMedia < 5 -> {
+//            print("¿El alumno se ha presentado a recuperación? (S/N)")
+//            val respuesta : String? = readLine()?.uppercase()
+//
+//            when (respuesta) {
+//                "S" -> "En recuperación"
+//                "N" -> "Suspenso"
+//                else -> "Respuesta no esperada"
+//            }
+//        }
+//        else -> "Nota no esperada"
+//    }
+//
+//    println("Situación del alumno: $mensaje")
+//
+//}
