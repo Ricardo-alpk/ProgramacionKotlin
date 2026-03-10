752ddea3b04f055d74eae201a43b03d65c78ed1a
ricardo_alpk <ricardo.man.ramirez@gmail.com>
2026-03-10 17:02:37 +0100

    Actualización de archivos

--- src/PRACTICA8/ControlFlujo/WHILE.kt
+++ src/PRACTICA8/ControlFlujo/WHILE.kt
@@ -1,27 +1,40 @@
 package PRACTICA8.ControlFlujo
 
+import java.sql.SQLOutput
+
 fun main() {
-    var tiradas = 0
-
-    while (tiradas < 3){
-        println("OK, INTENTO NUMERO ${tiradas}")
-        tiradas++
-    }
-
     //Ejercicio planteado
     //Programa que pida un numero al usuario constantemente
     //y que siga hasta que se introduzca un cero
 
     //Ejemplo while:
 
-    var final = (readLine()?: "0").toInt()
+    println("INTRODUCE UN NUMERO")
+    try {
 
-    while (final != 0 ){
-        println("Introduce un numero")
-        final = (readLine()?: "0").toInt()
+
+        var numStop: Int = (readLine() ?: "0").toInt()
+
+        while (numStop != 0) {
+            println("INCORRECTO PRUEBA OTRA VEZ")
+            numStop = (readLine() ?: "0").toInt()
+
+        }
+
+        //ejercicio do-while en kotlin
+
+        println("==========EJERCICIO VERSIÓN DO-WHILE==========")
+
+        println("Lets Go, introduce un numero")
+        var numStop2: Int = (readLine() ?: "0").toInt()
+
+        do {
+            println("INCORRECTO, VUELVE A INTENTAR")
+            numStop2 = (readLine() ?: "0").toInt()
+        } while (numStop2 != 0)
+    } catch ( e: NumberFormatException ){
+        println("DEBES INTRODUCIR UNICAMENTE NUMEROS ENTEROS")
     }
-
-
 
 
 
